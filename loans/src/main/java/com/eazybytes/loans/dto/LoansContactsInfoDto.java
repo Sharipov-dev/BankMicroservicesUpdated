e717aa3efe1e2b770a087b015f3eb6f9c663a2c8
Arthur Sharipov <sharipov@sheridancollege.ca>
2024-09-22 03:23:11 -0400

    automatic config pulling up once changes are made

--- loans/src/main/java/com/eazybytes/loans/dto/LoansContactsInfoDto.java
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactsInfoDto.java
@@ -1,10 +1,17 @@
 package com.eazybytes.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactsInfoDto(String message, Map<String, String> contactDetails, List<String> OnCallSupport) {
+@Getter
+@Setter
+public class LoansContactsInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
